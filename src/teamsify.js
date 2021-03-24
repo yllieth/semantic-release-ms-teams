458057097d4ada557107e05ddfd4f1525a782223
Sylvain RAGOT <sylvain.ragot@hey.com>
2021-03-24 14:04:00 -0400

    fix(parsing): improved section detection
    #3

--- src/teamsify.js
+++ src/teamsify.js
@@ -12,150 +12,6 @@ const mdOptions = {
 }
 
 /**
- * Analyze the JSON built by remark from the release notes markdown text, and return the position of the given title if
- * present, -1 otherwise
- *
- * This JSON looks like that:
- * {
- *   type: 'root',
- *   children: [
- *     { type: 'heading', depth: 1, children: [Array], position: [Object] },
- *     { type: 'heading', depth: 3, children: [Array], position: [Object] },                                : ### Bug Fixes
- *     { type: 'list', ordered: false, start: null, spread: false, children: [Array], position: [Object] }, : list of changes
- *     { type: 'heading', depth: 3, children: [Array], position: [Object] },                                : ### Features
- *     { type: 'list', ordered: false, start: null, spread: false, children: [Array], position: [Object] }, : list of changes
- *     { type: 'heading', depth: 3, children: [Array], position: [Object] },                                : BREAKING CHANGES
- *     { type: 'list', ordered: false, start: null, spread: false, children: [Array], position: [Object] }, : list of changes
- *     }
- *   ],
- *   position: { start: { line: 1, column: 1, offset: 0 }, end: { line: x, column: y, offset: z } }
- * }
- *
- * @param tree
- * @param title
- * @returns {number}
- */
-const titlePosition = (tree, title) => {
-  let position = -1
-  tree.children.forEach((child, index) => {
-    if (child.type === 'heading' && child.depth > 1 && child.children.length > 0) {
-      child.children.forEach(grandChild => {
-        if (grandChild.type === 'text' && grandChild.value === title) {
-          position = index
-        }
-      })
-    }
-  })
-
-  return position
-}
-
-/**
- * Analyze the JSON built by remark from the release notes markdown text, and return the markdown corresponding to the
- * Bug Fixes section of it.
- *
- * The Bug Fixes section, if present, is the first one. This function works on a subtree delimited by:
- * - start = the beginning of the tree
- * - end =
- *   - if present, the position of the Features section
- *   - if there is no Features section, and if present, the position of the BREAKING CHANGE section
- *   - else, the end of the tree
- *
- * @param tree A JSON object representing the markdown of the release notes
- * @returns {string} A markdown string
- */
-const extractBugfixes = (tree) => {
-  const bugfixesIndex = titlePosition(tree, 'Bug Fixes')
-  let bugfixesTree = []
-
-  if (bugfixesIndex > -1) {
-    const featuresIndex = titlePosition(tree, 'Features')
-    const breakingIndex = titlePosition(tree, 'BREAKING CHANGES')
-    const start = bugfixesIndex
-    let end = tree.children.length
-    if (breakingIndex > -1) { end = breakingIndex }
-    if (featuresIndex > -1) { end = featuresIndex } // must be after breakingIndex
-    const subTree = tree.children.slice(start, end)
-    if (subTree.length >= 1 && subTree[1].type === 'list') {
-      // eslint-disable-next-line prefer-destructuring
-      bugfixesTree = subTree[1]
-    }
-  }
-
-  return bugfixesTree.length > 0 ? toMarkdown({ type: 'root', children: [bugfixesTree] }, mdOptions) : undefined
-}
-
-/**
- * Analyze the JSON built by remark from the release notes markdown text, and return the markdown corresponding to the
- * Features section of it.
- *
- * The Features section, if present, is the second one. This function works on a subtree delimited by:
- * - start = the beginning of the Features section (a "heading" entry at the root, with the text "Features")
- * - end =
- *   - if present, the position of the BREAKING CHANGE section
- *   - else, the end of the tree
- *
- * @param tree A JSON object representing the markdown of the release notes
- * @returns {string} A markdown string
- */
-const extractFeatures = (tree) => {
-  const featuresIndex = titlePosition(tree, 'Features')
-  let featuresTree = []
-
-  if (featuresIndex > -1) {
-    const breakingIndex = titlePosition(tree, 'BREAKING CHANGES')
-    const start = featuresIndex
-    let end = tree.children.length
-    if (breakingIndex > -1) { end = breakingIndex }
-    const subTree = tree.children.slice(start, end)
-    if (subTree.length >= 1 && subTree[1].type === 'list') {
-      // eslint-disable-next-line prefer-destructuring
-      featuresTree = subTree[1]
-    }
-  }
-
-  return featuresTree.length > 0 ? toMarkdown({ type: 'root', children: [featuresTree] }, mdOptions) : undefined
-}
-
-/**
- * Analyze the JSON built by remark from the release notes markdown text, and return the markdown corresponding to the
- * BREAKING CHANGES section of it.
- *
- * The BREAKING CHANGES section, if present, is the last one. This function works on a subtree delimited by:
- * - start = the beginning of the BREAKING CHANGE section (a "heading" entry at the root, with the text "BREAKING CHANGE")
- * - end = the end of the tree
- *
- * @param tree A JSON object representing the markdown of the release notes
- * @returns {string} A markdown string
- */
-const extractBreaking = (tree) => {
-  const breakingIndex = titlePosition(tree, 'BREAKING CHANGES')
-  let breakingTree = []
-
-  if (breakingIndex > -1) {
-    const start = breakingIndex
-    const end = tree.children.length
-    const subTree = tree.children.slice(start, end)
-    if (subTree.length >= 1 && subTree[1].type === 'list') {
-      // eslint-disable-next-line prefer-destructuring
-      breakingTree = subTree[1]
-    }
-  }
-
-  return breakingTree.length > 0 ? toMarkdown({ type: 'root', children: [breakingTree] }, mdOptions) : undefined
-}
-
-const releaseSections = (context) => {
-  const tree = remark.parse(context.nextRelease.notes)
-
-  return {
-    bugfixes: extractBugfixes(tree),
-    features: extractFeatures(tree),
-    breaking: extractBreaking(tree),
-  }
-}
-
-/**
  * Return a JSON object meant to be sent to teams via a webhook. This object does not contain the details to the release
  * notes, but just the generic part.
  *
@@ -193,28 +49,64 @@ const baseMessage = (context) => {
   }
 }
 
+/**
+ * Converts the markdown text of the release notes into a JSON object, and go over it to extract sections (title with
+ * version number is ignored).
+ *
+ * Here is an example of a tree:
+ *
+ * {
+ *   type: 'root',
+ *   children: [
+ *     { type: 'heading', depth: 2, children: [], position: {} },                                                      // >> ignored
+ *     { type: 'heading', depth: 3, children: [{ type: 'text', value: 'Bug Fixes', position: {}}], position: {} },     // >> extract children's value
+ *     { type: 'list', ..., children: [], position: {} },                                                              // >> convert into markdown
+ *     { type: 'heading', depth: 3, children: [{ type: 'text', value: 'Features', position: {}}], position: {} },      // >> extract children's value
+ *     { type: 'list', ..., children: [], position: {} },                                                              // >> convert into markdown
+ *   ]
+ * }
+ *
+ * The result will be:
+ *
+ * [
+ *   { name: 'Bug Fixes', changes: '...'},
+ *   { name: 'Features', changes: '...'},
+ * ]
+ */
+const extractSections = (context) => {
+  const tree = remark.parse(context.nextRelease.notes)
+  const sections = []
+
+  /* eslint-disable-next-line no-plusplus */
+  for (let i = 0 ; i < tree.children.length - 1 ; i++) {
+    const child = tree.children[i]
+    const nextChild = tree.children[i + 1]
+    if (
+      child.type === 'heading' && child.depth === 3 && child.children[0].type === 'text' && // child is a section
+      nextChild.type === 'list' && nextChild.children.length > 0 // next child is a list of changes
+    ) {
+      sections.push({
+        name: child.children[0].value,
+        changes: toMarkdown({ type: 'root', children: [nextChild] }, mdOptions)
+      })
+    }
+  }
+
+  return sections
+}
+
 module.exports = (context) => {
-  const sections = releaseSections(context)
+  const sections = extractSections(context)
   const teamsMessage = baseMessage(context)
 
-  if (sections.bugfixes || sections.features || sections.breaking) {
+  if (sections.length > 0) {
     teamsMessage.sections.push({ text: '---' })
   }
 
-  if (sections.bugfixes) {
-    teamsMessage.sections.push({ text: '## Bug Fixes' })
-    teamsMessage.sections.push({ text: sections.bugfixes.replace('\n-', '\r-') })
-  }
-
-  if (sections.features) {
-    teamsMessage.sections.push({ text: '## Features' })
-    teamsMessage.sections.push({ text: sections.features.replace('\n-', '\r-') })
-  }
-
-  if (sections.breaking) {
-    teamsMessage.sections.push({ text: '## Breaking Changes' })
-    teamsMessage.sections.push({ text: sections.breaking.replace('\n-', '\r-') })
-  }
+  sections.forEach(section => {
+    teamsMessage.sections.push({ text: `## ${section.name}` })
+    teamsMessage.sections.push({ text: section.changes.replace('\n-', '\r-') })
+  })
 
   return teamsMessage
 }
