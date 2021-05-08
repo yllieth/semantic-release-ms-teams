38b4fcad5c22fa0a15f2a1acb690fd318b49b164
Sylvain RAGOT <sylvain.ragot@hey.com>
2021-05-07 20:52:36 -0400

    fix: success script isnt async
    Fixes #12

--- src/success.js
+++ src/success.js
@@ -1,12 +1,12 @@
 const fetch = require('node-fetch')
 const teamsify = require('./teamsify')
 
-module.exports = async (pluginConfig, context) => {
+module.exports = (pluginConfig, context) => {
   const { logger, env } = context
   const { webhookUrl } = pluginConfig
   const url = webhookUrl || env.TEAMS_WEBHOOK_URL
   const headers = { 'Content-Type': 'application/json' }
-  const body = await JSON.stringify(teamsify(pluginConfig, context))
+  const body = JSON.stringify(teamsify(pluginConfig, context))
 
   fetch(url, { method: 'post', body, headers})
     .then(() => logger.log('Message sent to Microsoft Teams'))
