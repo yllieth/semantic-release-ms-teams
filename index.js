26052a0146b1fddabcec416e1f5a0573633fa930
Sylvain RAGOT <sylvain.ragot@hey.com>
2022-07-13 20:03:03 -0400

    feat(teamsify): added dry-run support (can notify teams in dry-run mode)

--- index.js
+++ index.js
@@ -2,16 +2,38 @@ const lifecycleVerifyConditions = require('./lib/lifecycle-verify-conditions')
 const lifecycleSuccess = require('./lib/lifecycle-success')
 const canNotify = require('./lib/canNotify')
 
-let verified
+let verified = false
+
+/**
+ * Ensures the presence of the webhook URL to publish to.
+ */
 const verifyConditions = (pluginConfig, context) => {
   lifecycleVerifyConditions(pluginConfig, context)
   verified = true
 }
 
-const success = (pluginConfig, context) => {
+/**
+ * Support dry-run mode by eventually sending release notes to MS Team.
+ */
+const generateNotes = async (pluginConfig, context) => {
+  const { options } = context
+  const isDryRunMode = options.dryRun === true
+  const notifyInDryRun = pluginConfig.notifyInDryRun ?? true
+
+  if (isDryRunMode && notifyInDryRun) {
+    await success(pluginConfig, context)
+  }
+}
+
+/**
+ * Send release note to MS Teams
+ *
+ * This step will be skipped in dry-run mode.
+ */
+const success = async (pluginConfig, context) => {
   if (verified && canNotify(context)) {
     await lifecycleSuccess(pluginConfig, context)
   }
 }
 
-module.exports = { verifyConditions, success }
+module.exports = { verifyConditions, generateNotes, success }
