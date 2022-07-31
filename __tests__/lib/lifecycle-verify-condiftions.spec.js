61ec4f84923cca410a89f814644dbe88fba6704e
Sylvain RAGOT <sylvain.ragot@hey.com>
2022-07-30 21:02:14 -0400

    test(lib/lifecycle-verify-conditions): added unit tests

---
+++ __tests__/lib/lifecycle-verify-condiftions.spec.js
@@ -0,0 +1,69 @@
+const lifecycleVerifyConditions = require('../../lib/lifecycle-verify-conditions')
+
+describe('lifecycleVerifyConditions', () => {
+  let pluginConfig
+  let context
+
+  beforeEach(() => {
+    pluginConfig = { webhookUrl: undefined }
+    context = { env: { TEAMS_WEBHOOK_URL: undefined } }
+  })
+
+  it('throws an error if there are no webhook URL defined', () => {
+    try {
+      // act
+      lifecycleVerifyConditions(pluginConfig, context)
+    } catch (e) {
+      // assert
+      expect(e.name).toBe('AggregateError')
+      expect(e._errors[0].message).toBe('Invalid WebHook URL')
+    }
+
+    // assume
+    expect.assertions(2)
+  })
+  it('throws an error if the webhook URL is invalid (not a URL format)', () => {
+    // arrange
+    pluginConfig.webhookUrl = 'not an url'
+
+    try {
+      // act
+      lifecycleVerifyConditions(pluginConfig, context)
+    } catch (e) {
+      // assert
+      expect(e.name).toBe('AggregateError')
+      expect(e._errors[0].message).toBe('Invalid WebHook URL')
+    }
+
+    // assume
+    expect.assertions(2)
+  })
+  it('throws an error if the webhook URL is invalid (empty string)', () => {
+    // arrange
+    pluginConfig.webhookUrl = ''
+
+    try {
+      // act
+      lifecycleVerifyConditions(pluginConfig, context)
+    } catch (e) {
+      // assert
+      expect(e.name).toBe('AggregateError')
+      expect(e._errors[0].message).toBe('Invalid WebHook URL')
+    }
+
+    // assume
+    expect.assertions(2)
+  })
+  it('does not throw an error if the webhook URL is valid', () => {
+    // arrange
+    pluginConfig.webhookUrl = 'http://example.com'
+
+    try {
+      // act
+      lifecycleVerifyConditions(pluginConfig, context)
+    } catch (e) {
+      // assume
+      fail('Should no go there')
+    }
+  })
+})
